38987b058a9e3d38f481b888474f59ea7fa65048
gracetang1201 <gracetang1201@gmail.com>
2016-11-29 21:08:00 -0500

    Finished Book.java

--- 01Library/Book.java
+++ 01Library/Book.java
@@ -3,6 +3,11 @@ public class Book{
     private String title;
     private String ISBN;
 
+    public static void main (String[]args){
+	Book X = new Book("Me", "All About Me", "123");
+	System.out.println(X);
+    }
+
     public Book(){
     }
 
@@ -19,5 +24,17 @@ public class Book{
     }
     public String getISBN(){
 	return ISBN;
+    }
+    public void setAuthor(String sAuth){
+	author = sAuth;
+    }
+    public void setTitle(String sTitle){
+	title = sTitle;
+    }
+    public void setISBN(String sISBN){
+	ISBN = sISBN;
+    }
+    public String toString(){
+	return "Title: " + title + ", Author: " + author + ", ISBN: " + ISBN;
     }
 }
